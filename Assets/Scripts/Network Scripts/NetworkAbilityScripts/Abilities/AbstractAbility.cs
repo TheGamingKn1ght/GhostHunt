a409ef012f9bfd4ae316bd408a281acb9d79b704
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2026-06-19 15:24:47 -0400

    Make Abstract Ability and Abstract class

--- Assets/Scripts/Network Scripts/NetworkAbilityScripts/Abilities/AbstractAbility.cs
+++ Assets/Scripts/Network Scripts/NetworkAbilityScripts/Abilities/AbstractAbility.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using Unity.Netcode;
 
-public class AbstractAbility : NetworkBehaviour
+public abstract class AbstractAbility : NetworkBehaviour
 {
     //Create base profile of an ability
 }
+
